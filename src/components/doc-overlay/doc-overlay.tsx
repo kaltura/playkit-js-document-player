4538f725192525e84561c40e80542101dd6fd537
Serhii Marchenko <serhii.marchenko@kaltura.com>
2024-01-31 15:15:08 +0200

    fix(FEC-13672): use source metadata to display doc name

--- src/components/doc-overlay/doc-overlay.tsx
+++ src/components/doc-overlay/doc-overlay.tsx
@@ -8,11 +8,11 @@ import * as styles from './doc-overlay.scss';
 const { withText, Text } = ui.preacti18n;
 
 const translates = {
-  previewText: <Text id="docPlayer.previewText">Click to view Supply & Demand</Text>,
+  previewText: <Text id="docPlayer.previewText">Click to view</Text>,
   previewButtonText: <Text id="docPlayer.previewButtonText">View document</Text>,
-  downloadText: <Text id="docPlayer.downloadText">Supply & Demand is unavailable, download to view.</Text>,
+  downloadText: <Text id="docPlayer.downloadText">is unavailable, download to view.</Text>,
   downloadButtonText: <Text id="docPlayer.downloadButtonText">Download document</Text>,
-  contentUnavailableText: <Text id="docPlayer.contentUnavailableText">Supply & Demand is unavailable to view.</Text>
+  contentUnavailableText: <Text id="docPlayer.contentUnavailableText">is unavailable to view.</Text>
 };
 
 export interface DocumentOverlayProps {
@@ -23,25 +23,26 @@ export interface DocumentOverlayProps {
   downloadText?: string;
   downloadButtonText?: string;
   contentUnavailableText?: string;
+  sourceName?: string;
 }
 
 export const DocumentOverlay = withText(translates)((props: DocumentOverlayProps) => {
   const getContent = (): { text: string; buttonText?: string; onClick?: () => void } => {
     if (props.onPreview) {
       return {
-        text: props.previewText!,
+        text: `${props.previewText!} ${props.sourceName}`,
         buttonText: props.previewButtonText!,
         onClick: props.onPreview
       };
     } else if (props.onDownload) {
       return {
-        text: props.downloadText!,
+        text: `${props.sourceName} ${props.downloadText!}`,
         buttonText: props.downloadButtonText!,
         onClick: props.onDownload
       };
     }
     return {
-      text: props.contentUnavailableText!
+      text: `${props.sourceName} ${props.contentUnavailableText!}`
     };
   };
 
